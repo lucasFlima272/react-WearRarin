bc6e958ea051fc0d22b36bf52a06147f9eaff9c8
LUCAS FERREIRA LIMA <24228128807@info.local>
2025-12-04 17:29:38 -0300

    fazendo react WearRarin

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,37 +1,66 @@
 import { Nav, Navbar } from 'react-bootstrap';
 import './Header.css';
+import react, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Header() {
+    const [pesquisa, setPesquisa] = useState<string>("")
+    const Navigate = useNavigate();
+
+    const handleSearch = () => {
+        if (!pesquisa.trim()) return;
+
+        Navigate(`/produtos/pesquisa?query=${encodeURIComponent(pesquisa)}`);
+        setPesquisa("");
+
+    }
+
+    const handleKeyDown = (evento: React.KeyboardEvent<HTMLInputElement>) => {
+        if (evento.key === 'Enter') {
+            handleSearch();
+            
+        }
+    }
+
+
+
+
     return (
-            <header>
-        <Navbar expand="md" className="container_cabecalho">
-          <Navbar.Collapse id= 'basic-navbar-nav'>
-            <Nav className='me-auto conteudo-nav'>
-                
-           
-
-            <Nav.Item className="box_busca" id="box_busca">
-                <svg className="icone_lupa" xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 512 512">
-                    <path fill="currentColor"
-                        d="M368 208a160 160 0 1 0 -320 0 160 160 0 1 0 320 0zM337.1 371.1C301.7 399.2 256.8 416 208 416 93.1 416 0 322.9 0 208S93.1 0 208 0 416 93.1 416 208c0 48.8-16.8 93.7-44.9 129.1L505 471c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L337.1 371.1z" />
-                </svg>
-                <input className="campo_busca" type="text" name="" id=""/>
-            </Nav.Item>
+        <header>
+            <Navbar expand="md" className="container_cabecalho">
+                <Navbar.Collapse id='basic-navbar-nav'>
+                    <Nav className='me-auto conteudo-nav'>
 
 
 
+                        <Nav.Item className="box_busca" id="box_busca">
+                            <svg className="icone_lupa" xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 512 512">
+                                <path fill="currentColor"
+                                    d="M368 208a160 160 0 1 0 -320 0 160 160 0 1 0 320 0zM337.1 371.1C301.7 399.2 256.8 416 208 416 93.1 416 0 322.9 0 208S93.1 0 208 0 416 93.1 416 208c0 48.8-16.8 93.7-44.9 129.1L505 471c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L337.1 371.1z" />
+                            </svg>
+                            <input className="campo_busca"
+                             type="text" 
+                              value={pesquisa} 
+                              onChange={p => setPesquisa(p.target.value)}
+                              onKeyDown={handleKeyDown}
+                              placeholder='pesquisar'
+                              />
+                        </Nav.Item>
 
-            <h1>WEAR RARIN</h1>
 
-        
-        </Nav>
-        </Navbar.Collapse>
-        <Navbar.Toggle aria-controls="basic-navbar" className="ms-auto" />
-        </Navbar>
-    </header>
-    
+
+
+                        <h1>WEAR RARIN</h1>
+
+
+                    </Nav>
+                </Navbar.Collapse>
+                <Navbar.Toggle aria-controls="basic-navbar" className="ms-auto" />
+            </Navbar>
+        </header>
+
     )
-        
-    
+
+
 }
--- src/pages/Produtos/Produtos.tsx
+++ src/pages/Produtos/Produtos.tsx
@@ -11,46 +11,75 @@ import { getWearRarin } from '../../services/WearRarinService';
 import CardProduto from '../../components/CardProduto/CardProduto';
 import Carrosel from '../../components/Carrosel/Carrosel';
 import Header from '../../Header/Header';
+import { useLocation } from 'react-router-dom';
 
 export default function Produtos() {
 
     const [WearRarin, setWearRarin] = useState<WearRarin[]>([]);
+    const location = useLocation();
+
+    const parametrosPesquisados = new URLSearchParams(location.search);
+    const termo_pesquisado = parametrosPesquisados.get('query');
+
+
 
     const fetchWearRarin = async () => {
         try {
             const dados = await getWearRarin();
-            console.log("Lista de camisas vinda da api", dados);
-            setWearRarin(dados);
+
+            if (termo_pesquisado) {
+                const dados_filtrados = dados.filter(w =>
+                    w.nome.toLowerCase().includes(termo_pesquisado.toLowerCase()) ||
+                    w.descricao.toLowerCase().includes(termo_pesquisado.toLowerCase()) ||
+                    w.categoria.some(cat => cat.toLowerCase().includes(termo_pesquisado.toLowerCase()))
+                );
+                setWearRarin(dados_filtrados);
+            } else
+         setWearRarin(dados);
         } catch (error) {
-            console.error("Erro ao executar getWearrarin:", error);
+            console.error("Erro ao executar getWearRarin", error);
         }
+
+
+            console.log("Lista de camisas vinda da api");
+           
+        setWearRarin
     }
 
     useEffect(() => {
         fetchWearRarin();
-    }, [])
+        console.log("Termo pesquisado", termo_pesquisado);
+    }, [termo_pesquisado])
 
     return (
         <>
-           <Header/>
+            <Header />
             <main>
 
                 <Carrosel />
 
-                <section className="container">
-
+                <section className="container_produtos">
+                    <h1 className="acessivel"> Camisa OVERSIZED </h1>
                     <div className="titulo">
-                        <span>OVERSIZED</span>
-                    </div>
+                    <span>
+                    
+                       {
 
+                         termo_pesquisado ? `Resultados para: ${termo_pesquisado}`: "Nome da categoria"
+
+                       }
+
+                    </span>
+                    </div>
+                   
                     <section className="cards">
 
                         {
                             WearRarin.map((b: WearRarin) => (
                                 <CardProduto
-                                imagens={b.imagens [0] ?? ""}
-                                nome={b.nome}
-                                preco={b.preco}
+                                    imagens={b.imagens[0] ?? ""}
+                                    nome={b.nome}
+                                    preco={b.preco}
                                 />
                             ))
                         }
